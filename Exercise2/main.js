2dcf6504c28f6e51284d5dfc77db670667eb4b99
ferran <lferran@student.ethz.ch>
2016-04-05 12:26:23 +0200

    almost finished dinamyc icons

--- Exercise2/main.js
+++ Exercise2/main.js
@@ -1,25 +1,44 @@
-/**/
-
-/* scrollTimer is a setTimeout method that will call the updateMenu function after 2000ms */
-var scrollTimer = setTimeout(updateMenu, 2000);
+/*======= Initial Variables =========*/
+/* scrollTimer is a Timer that will call the updateMenu() after 2000ms */
+var scrollTimer = setTimeout(updateMenuSizes, 2000);
 
 /* Array where the different section ids are saved */
 var sections = new Array('home', 'about', 'portfolio', 'blog', 'contact');
 
 /* Array for the current section frequencies*/
-var sf = new Array ();
+var SF = new Array ();
+//Initialize it
+initSF();
 
 /* Variable with the current maximum frequency */
-var mf = 0;
+var MF = 0;
 
 /* Icon size limits*/
 var imgMax = 70;
 var imgMin = 30;
 
 
+/*======= Main loop =========*/
+$(document).ready(
+    // This function is called when the DOM is ready
+    function(){
+        // Every time we scroll, we clear the scrollTimer
+        $(window).scroll(function(e) {clearTimeout(scrollTimer); scrollTimer = setTimeout(updateMenuSizes, 2000);});
+    }
+);
+
+
+/*======= Functions =========*/
+
+/* Initializes the section frequencies array SF */
+function initSF() {
+    for (i = 0; i < sections.length; i++) {
+        SF[sections[i]] = 0;
+    }
+}
+
+/* Returns true if current scroll position is in identifier section, and false otherwise */
 function isInSection(identifier, scrollPosition){
-    /* Function that says if current scroll
-       position is in identifier section */
     var section = $("#" + identifier);
     if (scrollPosition >= section.position().top && scrollPosition <= (section.position().top + section.outerHeight())){
         return true;
@@ -27,7 +46,38 @@ function isInSection(identifier, scrollPosition){
     return false;
 }
 
+/* Increment frequency for section, and update MF if necessary */
+function refreshMF(identifier) {
+    SF[identifier]++;
+    if (SF[identifier] > MF) {
+        MF = SF[identifier];
+    }
+}
 
+/* Resizes Icon size according to given forula */
+function modifyIconSize(identifier) {
+    var newSize = (SF[identifier] / MF) * (imgMax - imgMin) + imgMin;
+    $('#' + identifier + "-icon").css('width', newSize + "px");
+}
 
-
-
+/* Main function that updates the menu sizes */
+function updateMenuSizes() {
+    // Get center position of the current viewport
+    var currentScrollPosition = $(window).scrollTop() + ($( window ).height()/2) + $('#menu').height();
+    
+    // Iterate through sections to check if MF changed
+    for (i=0; i < sections.length; i++){
+        if (isInSection(sections[i], currentScrollPosition)){
+            refreshMF(sections[i]);
+        }
+    }
+    
+    // Iterate sections and change to new sizes
+    for (i = 0; i < sections.length; i++) {
+        modifyIconSize(sections[i]);
+    }
+    
+    // Re-start scrollTimer
+    clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(updateMenuSizes, 2000);
+}
